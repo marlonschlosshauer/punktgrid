d070e9b3205e2e06f75f6d04c1a04ead165b2822
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2023-09-16 23:39:45 +0200

    Fix tailwind randomly deciding to work different

--- src/components/dot/Dot.tsx
+++ src/components/dot/Dot.tsx
@@ -4,10 +4,13 @@ export interface DotProps {
   active?: boolean;
 }
 
-export const Dot: React.FC<DotProps> = ({ active = false }) => (
-  <div
-    className={`w-[1rem] h-[1rem] border-4 border-gray-300 ${
-      active && "border-blue-300"
-    } hover:border-green-400 active:border-green-400 rounded-full bg-gray-200 hover:bg-green-300 active:bg-green-300 ease-in-out duration-200`}
-  />
-);
+export const Dot: React.FC<DotProps> = ({ active = false }) => {
+  console.log(active);
+  return (
+    <div
+      className={`w-[1rem] h-[1rem] border-4 ${
+        active ? "border-blue-300" : "border-gray-300"
+      } hover:border-green-400 active:border-green-400 rounded-full bg-gray-200 hover:bg-green-300 active:bg-green-300 ease-in-out duration-200`}
+    />
+  );
+};
